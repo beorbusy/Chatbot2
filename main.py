b1f654c5aecbf466ca5a837d5ba659a967d85988
beorbusy <beorbusy@gmail.com>
2024-10-02 02:49:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-from flask import Flask, render_template, request, jsonify
-import os
-from one import get_message, chatbot_response  # Import the chatbot response function
+# main.py
+
+from flask import Flask, render_template
+from one import chatbot  # Importing the chatbot function from one.py
 
 app = Flask(__name__)
 
-port = int(os.getenv('PORT', 4000))
-
 @app.route('/')
-def hello():
-    message = get_message()
-    return render_template('index.html', message=message)
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    user_message = request.form.get('user_message')  # Get user message from form data
-    response = chatbot_response(user_message)  # Get response from the chatbot
-    return jsonify({'response': response})  # Return response as JSON
+def home():
+    return render_template('index.html')  # Render index.html
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)  # Run the app in debug mode
